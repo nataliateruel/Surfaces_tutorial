ab1d9f2843eb08cce2f2bbcfd22acc8513b37041
nataliateruel <56839341+nataliateruel@users.noreply.github.com>
2023-02-21 13:50:14 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Surfaces'
+copyright = '2023, Najmanovich Research Group'
+author = 'Natalia F B Teruel'
 
 release = '0.1'
 version = '0.1.0'
